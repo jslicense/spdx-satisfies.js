c33b31be1e8343ad16eb369865745691c1b2a2d0
Kyle E. Mitchell <kyle@kemitchell.com>
2016-07-15 15:46:10 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,58 +1,77 @@
 var parse = require('spdx-expression-parse')
 var compare = require('spdx-compare')
+var ranges = require('spdx-ranges')
 
-module.exports = (function() {
-  var rangesAreCompatible = function(first, second) {
+module.exports = (function () {
+  var rangesAreCompatible = function (first, second) {
     return (
       first.license === second.license ||
-      ranges.some(function(range) {
+      ranges.some(function (range) {
         return (
           range.indexOf(first.license) > -1 &&
-          range.indexOf(second.license) ) }) ) }
+          range.indexOf(second.license)
+        )
+      })
+    )
+  }
 
-  var identifierInRange = function(identifier, range) {
+  var identifierInRange = function (identifier, range) {
     return (
       identifier.license === range.license ||
-      compare.gt(identifier.license, range.license) ) }
+      compare.gt(identifier.license, range.license)
+    )
+  }
 
-  var licensesAreCompatible = function(first, second) {
+  var licensesAreCompatible = function (first, second) {
     if (first.exception !== second.exception) {
-      return false }
-    else if (second.hasOwnProperty('license')) {
+      return false
+    } else if (second.hasOwnProperty('license')) {
       if (second.hasOwnProperty('plus')) {
         if (first.hasOwnProperty('plus')) {
           // first+, second+
-          return rangesAreCompatible(first, second) }
-        else {
+          return rangesAreCompatible(first, second)
+        } else {
           // first, second+
-          return identifierInRange(first, second) } }
-      else {
+          return identifierInRange(first, second)
+        }
+      } else {
         if (first.hasOwnProperty('plus')) {
           // first+, second
-          return identifierInRange(second, first) }
-        else {
+          return identifierInRange(second, first)
+        } else {
           // first, second
-          return first.license === second.license } } } }
+          return first.license === second.license
+        }
+      }
+    }
+  }
 
-  var recurseLeftAndRight = function(first, second) {
+  var recurseLeftAndRight = function (first, second) {
     var firstConjunction = first.conjunction
     if (firstConjunction === 'and') {
       return (
         recurse(first.left, second) &&
-        recurse(first.right, second) ) }
-    else if (firstConjunction === 'or') {
+        recurse(first.right, second)
+      )
+    } else if (firstConjunction === 'or') {
       return (
         recurse(first.left, second) ||
-        recurse(first.right, second) ) } }
+        recurse(first.right, second)
+      )
+    }
+  }
 
-  var recurse = function(first, second) {
+  var recurse = function (first, second) {
     if (first.hasOwnProperty('conjunction')) {
-      return recurseLeftAndRight(first, second) }
-    else if (second.hasOwnProperty('conjunction')) {
-      return recurseLeftAndRight(second, first) }
-    else {
-      return licensesAreCompatible(first, second) } }
+      return recurseLeftAndRight(first, second)
+    } else if (second.hasOwnProperty('conjunction')) {
+      return recurseLeftAndRight(second, first)
+    } else {
+      return licensesAreCompatible(first, second)
+    }
+  }
 
-  return function(first, second) {
-    return recurse(parse(first), parse(second)) } })()
-
+  return function (first, second) {
+    return recurse(parse(first), parse(second))
+  }
+})()
