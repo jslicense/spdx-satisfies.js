e784625ac539cdce72ee6e25588c4de44098bacf
Jeff McAffer <jmcaffer@microsoft.com>
2019-03-01 19:58:33 -0800

    rebase to upstream

--- index.js
+++ index.js
@@ -129,7 +129,9 @@ function isANDCompatible (one, two) {
   })
 }
 
-function satisfies (first, second) {
+function satisfies (first, second, options) {
+  options = options || {}
+  parse = options.parse || parse
   var one = expand(normalizeGPLIdentifiers(parse(first)))
   var two = flatten(normalizeGPLIdentifiers(parse(second)))
   return one.some(function (o) { return isANDCompatible(o, two) })
