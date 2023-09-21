4aa7f818a8bc49624de96ee2bdfd3a71642b79be
Kyle E. Mitchell <kyle@kemitchell.com>
2023-09-21 12:15:28 -0700

    Test without Tape
    Current Tape does not support some of the very old versions we do.

--- test.js
+++ test.js
@@ -1,30 +1,51 @@
+var assert = require('assert')
 var examples = require('./examples.json')
 var satisfies = require('./')
-var tape = require('tape')
 
-function label(expression, approved) {
-  return JSON.stringify(expression) + ', ' + JSON.stringify(approved)
+var failed = false
+
+function write (string) { process.stdout.write(string) }
+
+function label (expression, approved) {
+  write('satisfies(' + JSON.stringify(expression) + ', ' + JSON.stringify(approved) + ')')
 }
 
 for (const [expression, approved] of examples.returnTrue) {
-  tape.test(label(expression, approved), t => {
-    t.assert(satisfies(expression, approved), 'returns true')
-    t.end()
-  })
+  label(expression, approved)
+  try {
+    assert(satisfies(expression, approved) === true)
+  } catch (error) {
+    failed = true
+    write(' did not return true\n')
+    continue
+  }
+  write(' returned true\n')
 }
 
 // False Examples
 for (const [expression, approved] of examples.returnFalse) {
-  tape.test(label(expression, approved), t => {
-    t.assert(!satisfies(expression, approved))
-    t.end()
-  })
+  label(expression, approved)
+  try {
+    assert(satisfies(expression, approved) === false)
+  } catch (error) {
+    failed = true
+    write(' did not return false\n')
+    continue
+  }
+  write(' returned false\n')
 }
 
 // Invalid License Arrays
 for (const [expression, approved] of examples.throwErrors) {
-  tape.test(label(expression, approved), t => {
-    t.throws(() => satisfies(expression, approved))
-    t.end()
-  })
+  label(expression, approved)
+  try {
+    satisfies(expression, approved)
+  } catch (error) {
+    write(' threw an exception\n')
+    continue
+  }
+  failed = true
+  write(' did not throw an exception\n')
 }
+
+process.exit(failed ? 1 : 0)
